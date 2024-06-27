fd6b5c4172801c6109e235e6f3efb8a439dd9ffd
Nathnael Chanyalew <nathnaelc5@gmail.com>
2024-06-27 11:38:21 -0400

    Add the logic to let the user to replace the grid

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const containerDiv = document.querySelector("#container");
+const adjustGridButton = document.querySelector("button");
 
-function createRowDivs(){
-    for(let i = 0; i < 16; i++){
+function createRowDivs(numberOfSquaresPerSide){
+    for(let i = 0; i < numberOfSquaresPerSide; i++){
         let rowDiv = document.createElement("div");
         rowDiv.setAttribute("id", `row-${i+1}`);
         containerDiv.appendChild(rowDiv);
@@ -19,15 +20,38 @@ function createSquare(square){
     return square;
 }
 
-function createGridOfSquares(){
-    for(let i = 0; i < 16; i++){
+function getNumberOfSquares(){
+    return Number(prompt("Enter the number of squares you want per side: ", ));
+}
+
+function removeSquaresFromGrid(){
+    containerDiv.innerHTML = "";
+}
+
+function addSquaresToGrid(numberOfSquaresPerSide){
+    for(let i = 0; i < numberOfSquaresPerSide; i++){
         let currentRowDiv = document.querySelector(`#row-${i+1}`);
-        for(let k = 0; k < 16; k++){
+        for(let k = 0; k < numberOfSquaresPerSide; k++){
             let squareDiv = createSquare(document.createElement("div"));
             currentRowDiv.appendChild(squareDiv);
         }
     }
 }
 
-createRowDivs();
-createGridOfSquares();
+function createGridOfSquares(numberOfSquaresPerSide){
+    removeSquaresFromGrid();
+
+    if(numberOfSquaresPerSide > 100){
+        createRowDivs(100);
+        addSquaresToGrid(100);
+    }else {
+        createRowDivs(numberOfSquaresPerSide);
+        addSquaresToGrid(numberOfSquaresPerSide);
+    }
+}
+
+createGridOfSquares(16);
+
+adjustGridButton.addEventListener("click", () => {
+    createGridOfSquares(getNumberOfSquares());
+});
